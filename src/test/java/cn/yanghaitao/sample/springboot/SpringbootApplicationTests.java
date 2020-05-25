4cac6fb4cfaa20a9b3e9c4b8b5b1a9426064ed1c
Poseidon1979 <paul.yang1979@gmail.com>
2020-05-25 14:04:32 +0800

    first commit

---
+++ src/test/java/cn/yanghaitao/sample/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.yanghaitao.sample.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
